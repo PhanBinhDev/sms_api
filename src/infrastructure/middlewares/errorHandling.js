bc343bfa250e1acca0272c92aba0b748d8449300
PhanBinhDev <phanbinh.dev@gmail.com>
2024-06-20 17:22:06 +0700

    fear-validate
    Add validate with Joi

--- src/infrastructure/middlewares/errorHandling.js
+++ src/infrastructure/middlewares/errorHandling.js
@@ -1,5 +1,4 @@
 const { StatusCodes } = require('http-status-codes')
-// import { env } from '~/config/environment'
 
 // Middleware xử lý lỗi tập trung trong ứng dụng Back-end NodeJS (ExpressJS)
 const errorHandlingMiddleware = (err, req, res, next) => {
@@ -14,7 +13,7 @@ const errorHandlingMiddleware = (err, req, res, next) => {
   }
 
   // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi. (Muốn hiểu rõ hơn hãy xem video 55 trong bộ MERN Stack trên kênh Youtube: https://www.youtube.com/@trungquandev)
-  if (env.NODE_ENV !== 'development') delete responseError.stack
+  if (process.env.NODE_ENV !== 'development') delete responseError.stack
 
   // Đoạn này có thể mở rộng nhiều về sau như ghi Error Log vào file, bắn thông báo lỗi vào group Slack, Telegram, Email...vv Hoặc có thể viết riêng Code ra một file Middleware khác tùy dự án.
   // ...
