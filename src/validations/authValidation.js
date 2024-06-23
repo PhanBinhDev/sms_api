4c559c8948620257f5717502b63139b3a19437c1
PhanBinhDev <phanbinh.dev@gmail.com>
2024-06-23 23:25:52 +0700

    Auth
    Other auth fear reset password

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -145,6 +145,19 @@ const schemaCreateUser = Joi.object({
     })
   })
 })
+
+const schemaResetPassword = Joi.object({
+  newPassword: Joi.string()
+    .required()
+    .min(6) // Tối thiểu 6 ký tự
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/) // Phải có ít nhất 1 chữ thường, 1 chữ hoa, 1 số
+    .message({
+      'string.empty': 'Mật khẩu không được để trống',
+      'any.required': 'Mật khẩu là bắt buộc',
+      'string.pattern.base':
+        'Mật khẩu phải có ít nhất 6 ký tự, bao gồm chữ thường, chữ hoa và số'
+    })
+})
 module.exports = {
   validateSignInWithCredentials: async (req, res, next) => {
     try {
@@ -169,6 +182,23 @@ module.exports = {
     } catch (error) {
       next(
         new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message)
+      )
+    }
+  },
+  validateResetPassword: async (req, res, next) => {
+    try {
+      await schemaResetPassword.validateAsync(
+        {
+          newPassword: req.body.newPassword
+        },
+        {
+          abortEarly: false
+        }
+      )
+      next()
+    } catch (err) {
+      next(
+        new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(err).message)
       )
     }
   }
